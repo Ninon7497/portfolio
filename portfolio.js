6286d0a7d75d7b8214f46fe8420c9079216c397b
Ninon Roche <rocheninon74@gmail.com>
2025-01-14 19:17:13 +0100

    ajout fichiers

---
+++ portfolio.js
@@ -0,0 +1,33 @@
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const offset = document.querySelector('.navbar').offsetHeight;
+
+        window.scrollTo({
+            top: targetElement.offsetTop - offset,
+            behavior: 'smooth'
+        });
+    });
+});
+
+const sections = document.querySelectorAll('.full-page');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - document.querySelector('.navbar').offsetHeight;
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    });
+});
